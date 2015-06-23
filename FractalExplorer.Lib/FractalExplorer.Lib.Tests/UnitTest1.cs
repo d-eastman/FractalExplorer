0cef3497e3cfae502f0b51a89d2cd97f17011a22
fuzzylikeagangster <fuzzylikeagangster@yahoo.com>
2015-06-23 09:31:52 -0400

    Initial checkin - empty lib, test, and winforms client projects

---
+++ FractalExplorer.Lib/FractalExplorer.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FractalExplorer.Lib.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
